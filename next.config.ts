13f4d11702c0474007131c89e65b3ae1bccb9d10
Farelart <ffjdganlaky@gmail.com>
2025-09-21 18:59:46 +0100

    landing page changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/eleven-public-cdn/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
